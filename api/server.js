8d0a1fed09bb1f94f2695df6be5560112fc7d9e1
rosslore <lorenzoross87@gmail.com>
2022-12-13 18:01:22 -0500

    progress COB

--- api/server.js
+++ api/server.js
@@ -10,8 +10,24 @@ app.use(express.json()).use(cors({
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true // enable set cookie
 }));
-const { hash, compare } = bcrypt;
+// const { hash, compare } = bcrypt;
 const SALTS = 12;
+
+const encryptPassword = async (password) => {
+    const hash = await bcrypt.hash(password, SALTS);
+    return hash;
+}
+
+const comparePassword = async (password, hash) => {
+    try {
+        const isMatch = await bcrypt.compare(password, hash);
+        return isMatch;
+    } catch (error) {
+        console.log(error)
+        throw "Error comparing password";
+    }
+
+}
 
 app.get('/', (req, res) => {
     res.set("Access-Control-Allow-Origin", "*").status(200).send('Got / 🙂');
@@ -57,15 +73,41 @@ app.get('/report', (req, res) => { // Display all Incidents from incident_report
         });
 });
 
-app.post('/login', async (req, res) => { // Authentication
-    let hashed = await (knex('member').where('username', req.body.username.toLowerCase()).select('password_hash'));
-    try {
-      hashed = hashed[0].password_hash;
-      compare(req.body.password, hashed)
-        .then(match =>  match ? res.status(200).end() : res.status(403).end())
-    } catch (e) {
-      res.status(500).end();
+// app.post('/login', async (req, res) => { // Authentication
+//     let hashed = await (knex('member').where('username', req.body.username.toLowerCase()).select('password_hash'));
+//     try {
+//       hashed = hashed[0].password_hash;
+//       compare(req.body.password, hashed)
+//         .then(match =>  match ? res.status(200).end() : res.status(403).end())
+//     } catch (e) {
+//       res.status(500).end();
+//     }
+//   });
+
+app.post('/login', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log('Cookies: ', req.cookies)
+    console.log('Session: ', req.session)
+    console.log('Body: ', req.body)
+    console.log('Signed Cookies: ', req.signedCookies)
+
+    const user = await knex('member')
+        .select('*')
+        .where('username', username)
+        .catch(err => { console.log(err) });
+    if (user.length > 0) {
+        bcrypt.compare(password, user[0].password, (error, response) => {
+            if (response) {
+                req.session.user = user;
+                console.log(req.session.user);
+                res.send({ message: "User authenticated" });
+            } else {
+                res.send({ message: "Username or password incorrect" });
+            }
+        });
     }
-  });
+
+})
 
 module.exports = app;
