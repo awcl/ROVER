3521fe21a590be45c3f503a3621e31fa8ca0daf9
awcl <113561942+awcl@users.noreply.github.com>
2022-12-22 08:17:14 -0700

    head

--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <div className="Header">
             <h1 className="headerContent" style={{ textAlign: "left" }}>&emsp;ROVER</h1>
+            {session.username && <h1 className="headerContent" style={{ textAlign: "center" }}>{session.admin ? <>Admin</> : <>Regular User</>}</h1>}
             {session.username && <h1 className="headerContent" style={{ textAlign: "right" }}>Hello {session.rank} {session.last_name}&emsp;</h1>}
         </div>
     )
